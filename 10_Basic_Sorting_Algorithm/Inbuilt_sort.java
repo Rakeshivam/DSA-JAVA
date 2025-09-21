a9d5b634d7c7e130a6e07137a1ba44006398e4be
Rakesh Kushwaha <rakeshkk589@gmail.com>
2025-09-21 11:28:06 +0530

    Sorting

---
+++ 10_Basic_Sorting_Algorithm/Inbuilt_sort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Inbuilt_sort {
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        // bubbleSort(arr);
+        Arrays.sort(arr);
+        Arrays.sort(arr, 0, 3);
+        // Arrays.sort(arr, Collections.reverseOrder()); // int = Integer
+        printArr(arr);
+    }
+}
